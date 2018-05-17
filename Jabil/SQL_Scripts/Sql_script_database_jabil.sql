28fed9ca929e05062272edf2caee228e1dff445e
Luis R Espinoza B <luis.espinoza@facware.com>
2018-05-16 22:41:54 -0600

    added scritp to generate database models

---
+++ Jabil/SQL_Scripts/Sql_script_database_jabil.sql
@@ -0,0 +1,44 @@
+﻿create database x;
+
+use x;
+
+
+create table Buildings(
+	PKBuilding int IDENTITY(1,1) PRIMARY KEY, 
+	Building nvarchar(100) NOT NULL
+);
+
+INSERT INTO Buildings(Building) VALUES ('A');
+INSERT INTO Buildings(Building) VALUES ('B');
+INSERT INTO Buildings(Building) VALUES ('C');
+INSERT INTO Buildings(Building) VALUES ('D');
+INSERT INTO Buildings(Building) VALUES ('E');
+
+create table Customers(
+	PKCustomers int IDENTITY(1,1) PRIMARY KEY, 
+	Customer nvarchar(100) NOT NULL,
+	Prefix nvarchar(5) NOT NULL,
+	FKBuilding int FOREIGN KEY REFERENCES Buildings(PKBuilding)
+);
+
+INSERT INTO Customers(Customer,Prefix,FKBuilding) VALUES ('John Mattensen','Mr',1);
+INSERT INTO Customers(Customer,Prefix,FKBuilding) VALUES ('Brat Losan','Mr',1);
+INSERT INTO Customers(Customer,Prefix,FKBuilding) VALUES ('John Mattensen','Mr',2);
+INSERT INTO Customers(Customer,Prefix,FKBuilding) VALUES ('Jessica Math','Eng',3);
+
+create table PartNumbers(
+	PKPartNumber int IDENTITY(1,1) PRIMARY KEY, 
+	PartNumber nvarchar(50) NOT NULL,	
+	FKCustomer int FOREIGN KEY REFERENCES Buildings(PKBuilding),
+	Available bit NOT NULL
+);
+
+INSERT INTO PartNumbers(PartNumber,FKCustomer,Available) VALUES ('ABC123',1,1);
+INSERT INTO PartNumbers(PartNumber,FKCustomer,Available) VALUES ('AB12C3',2,0);
+INSERT INTO PartNumbers(PartNumber,FKCustomer,Available) VALUES ('1A2B3C',3,0);
+INSERT INTO PartNumbers(PartNumber,FKCustomer,Available) VALUES ('123ABC',4,1);
+
+
+SELECT * FROM Buildings;
+SELECT * FROM Customers;
+SELECT * FROM PartNumbers;
