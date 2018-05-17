25368c7a07989573d3958aa1d707f338fea53507
Luis R Espinoza B <luis.espinoza@facware.com>
2018-05-17 11:59:15 -0600

    datatable added with left outer join

--- Jabil/Controllers/HomeController.cs
+++ Jabil/Controllers/HomeController.cs
@@ -3,26 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Jabil.Models;
+using Jabil.ViewModels;
 
 namespace Jabil.Controllers
 {
 	public class HomeController : Controller
 	{
+		private JabilModel _db;
+		public HomeController()
+		{
+			_db = new JabilModel();
+		}
+
 		public ActionResult Index()
 		{
-			return View();
+			IEnumerable<MainReport> data = (from part in _db.PartNumbers
+							   join customer in _db.Customers on part.FKCustomer equals customer.PKCustomers 
+							   into cp from partcustomer in cp.DefaultIfEmpty()
+							   join building in _db.Buildings on partcustomer.FKBuilding equals building.PKBuilding 
+							   into bg from buildingGroup in bg.DefaultIfEmpty()
+							   select new MainReport{
+								   NumberPart = part.PartNumber1,
+								   Availability = part.Available,
+								   Customer = partcustomer.Customer1 != null ? partcustomer.Customer1 : string.Empty,
+								   Building = buildingGroup.Building1 != null ? buildingGroup.Building1 : string.Empty
+							   });
+
+			/*IEnumerable<MainReport> data = (from part in _db.PartNumbers
+							   join customer in _db.Customers on part.FKCustomer equals customer.PKCustomers 
+							   into cp from partcustomer in cp.DefaultIfEmpty()
+							   join building in _db.Buildings on partcustomer.PKCustomers equals building.PKBuilding 
+							   into bg from buildingGroup in bg.DefaultIfEmpty()
+							   select new MainReport{
+								   NumberPart = part.PartNumber1,
+								   Availability = part.Available,
+								   Customer = partcustomer.Customer1,
+								   Building = buildingGroup.Building1
+							   });*/
+			return View(data);
 		}
 
 		public ActionResult About()
 		{
-			ViewBag.Message = "Your application description page.";
+			ViewBag.Message = "This application is just a test.";
 
 			return View();
 		}
 
 		public ActionResult Contact()
 		{
-			ViewBag.Message = "Your contact page.";
+			ViewBag.Message = "respinozabarboza@gmail.com.";
 
 			return View();
 		}
---
+++ Jabil/ViewModels/MainReport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jabil.ViewModels
+{
+	public class MainReport
+	{
+		public string NumberPart { get; set; }
+		public bool Availability { get; set; }
+		public string Customer { get; set; }
+		public string Building { get; set; }
+	}
+}
